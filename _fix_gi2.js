619de67203b4f9914e7888034b79c9bcafe96039
黄先生 <202507327@qq.com>
2026-06-22 15:35:09 +0800

    fix: 添加 content/ 目录到 git 以便 Cloudflare Pages 构建

---
+++ _fix_gi2.js
@@ -0,0 +1,6 @@
+﻿var fs = require("fs");
+var fp = "C:\\Users\\huang\\Desktop\\workspace\\SOP\\012-个人博客网站\\.gitignore";
+var c = fs.readFileSync(fp, "utf8");
+c = c.replace(/#\s*Content\s*\n\s*content\/\s*\n/, "");
+fs.writeFileSync(fp, c, "utf8");
+console.log("Done: " + c.includes("content/"));
---
+++ _fix_gitignore.js
@@ -0,0 +1,10 @@
+﻿var fs = require("fs");
+var path = require("path");
+var BASE = "C:\\Users\\huang\\Desktop\\workspace\\SOP\\012-个人博客网站";
+
+var fp = path.join(BASE, ".gitignore");
+var c = fs.readFileSync(fp, "utf8");
+// Remove content/ from gitignore so MDX files are tracked
+c = c.replace(/^\s*content\/$\n/m, "");
+fs.writeFileSync(fp, c, "utf8");
+console.log("Fixed .gitignore");
